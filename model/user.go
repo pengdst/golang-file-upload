ac6440eb578cdee360afe73a61fa012013516c3e
Pengkuh Dwi Septiandi <pengkuh@qiscus.chat>
2023-01-02 10:28:25 +0700

    fix!: change token with role in model user.go

--- model/user.go
+++ model/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	Name       string    `json:"name"`
 	Email      string    `json:"email"`
-	Token      string    `json:"token"`
+	Role       int       `json:"role"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdaetedAt time.Time `json:"updaeted_at"`
 }
